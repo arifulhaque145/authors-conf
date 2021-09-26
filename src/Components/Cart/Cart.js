d06247d1759ace96b4086ad137837ccac4683d7c
Ariful145 <arifulh145@gmail.com>
2021-09-26 18:44:17 +0600

    bug fixed

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,13 +1,22 @@
 import React from "react";
-import './Cart.css'
+import "./Cart.css";
 
 function Cart(props) {
+  const cart = document.getElementById("cart");
   const data = props.text;
-  let total = 0;
-  data.map(item => total+=parseInt(item.cost))
+  let total = 0,
+    height = 150;
+  data.map((item) => {
+    total += parseInt(item.cost);
+    height += 25;
+    cart.style.height = `${height}px`;
+  });
 
   return (
-    <div className="cart__container">
+    <div
+      id="cart"
+      className="cart__container stic shadow-sm p-3 mb-5 bg-white rounded"
+    >
       <h4>Ordered People {data.length}</h4>
       <ol>
         {data.map((item) => (
